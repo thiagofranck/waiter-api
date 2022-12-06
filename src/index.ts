b71c9047e826a55bf35a42fe18ee0dc479b48361
Thiago <thiagohenrique776@gmail.com>
2022-12-06 20:56:36 -0300

    chore: add mongodb config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
-});
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(3001, () => {
+      console.log(`Server is running on http://localhost${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no MongoDB'));
+
+
 
 
